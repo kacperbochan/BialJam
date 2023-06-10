744679f5d654b3d9089084c1674ffcbf83e4e3ef
bartekch1235 <82156135+bartekch1235@users.noreply.github.com>
2023-06-10 11:44:24 +0200

    simple ship movement
    el cringe

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -10,6 +10,8 @@ public class ShipMovement : MonoBehaviour
     public Vector2 windDirection = new Vector2(0, 0);
     public float windForce = 0;
     public float speed = 7;
+    public float rotation = 0;
+    public GameObject rotationPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +23,18 @@ public class ShipMovement : MonoBehaviour
     {
         float piDeegre = transform.rotation.eulerAngles.y / 180 * Mathf.PI;
         float moveX = Input.GetAxis("Vertical");
+        float moveY = Input.GetAxis("Horizontal");
         rigidBody.AddForce(new Vector3(windDirection.x * windForce, windDirection.y * windForce, 0), ForceMode.Acceleration);
+
         if (moveX != 0)
         {
             rigidBody.AddForce(new Vector3(Mathf.Cos(piDeegre)*speed*moveX, 0, -Mathf.Sin(piDeegre) * speed * moveX), ForceMode.Acceleration);
-
         }
+        if (moveY != 0) 
+        {
+            transform.RotateAround(rotationPoint.transform.position, Vector3.up * moveY, 10*Time.deltaTime);
+        }
+
         }
 
 }
