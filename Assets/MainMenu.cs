177a22a99342231b049a5ac99f34a5fa35b06df8
Tomasz Dąbrowski <51459629+DabToms@users.noreply.github.com>
2023-06-10 18:12:39 +0200

    Dodanie main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MAinMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        StartCoroutine(ChangeSceneWithDelay());
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    void Start()
+    {
+        //Start the coroutine we define below named ExampleCoroutine.
+    }
+
+    IEnumerator ChangeSceneWithDelay()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        // changed scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
+
