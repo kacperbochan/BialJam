be082d6bf06e4f1d309a40a3a01025fd721d3b0d
bartekch1235 <82156135+bartekch1235@users.noreply.github.com>
2023-06-10 15:53:45 +0200

    ship movement completed

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Floater : MonoBehaviour
 {
+    public bool right=true;
     public Rigidbody rigidbody;
     public float depthBeforeSubmerged = 1f;
     public float displacementAmount = 3f;
@@ -12,18 +13,41 @@ public class Floater : MonoBehaviour
     public float waterDrag = 0.99f;
     public float waterAngularDrag = 0.5f;
     public float offset ;
+    public float heightOffset;
     public GameObject parent;
+    public Transform startTransform;
 
     private void Start()
     {
         parent = transform.parent.gameObject;
-
+        startTransform = transform;
     }
 
     private void FixedUpdate()
     {
+        float shipRotation = parent.GetComponent<ShipMovement>().rotationForce;
+        if(right)
+        {
+            if(shipRotation> 0) 
+            {
+                displacementAmount= 1-shipRotation*0.7f;
+            }else
+            {
+                displacementAmount = 1f;
+            }
+        }
+        else
+        {
+            if(shipRotation < 0)
+            {
+                displacementAmount = 1 + shipRotation * 0.7f;
+            }else
+            {
+                displacementAmount = 1f;
+            }
+        }
         rigidbody.AddForceAtPosition(Physics.gravity/floaterCount, transform.position, ForceMode.Acceleration);
-        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x+offset+parent.transform.position.x);
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x + offset + parent.transform.position.x);
         if (transform.position.y < waveHeight)
         { 
             float displacementMultiplier = Mathf.Clamp01((waveHeight-transform.position.y) / depthBeforeSubmerged) * displacementAmount;
--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -7,10 +7,21 @@ public class ShipMovement : MonoBehaviour
 {
     public Rigidbody rigidBody;
     public float offset = 0;
+
+    public float rotationForce = 0;
+
+    //pasywny ruch ³odki
     public Vector2 windDirection = new Vector2(0, 0);
     public float windForce = 0;
-    public float speed = 7;
-    public float rotation = 0;
+
+
+
+    //aktywny ruch ³ódki
+    public float roatationSpeed = 7;//maxymalna predkosc skretu
+    public float maxSpeed = 10;
+
+    public float velocity = 0;
+    public float anchor = 0;
     public GameObject rotationPoint;
     // Start is called before the first frame update
     void Start()
@@ -21,19 +32,29 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        float piDeegre = transform.rotation.eulerAngles.y / 180 * Mathf.PI;
-        float moveX = Input.GetAxis("Vertical");
-        float moveY = Input.GetAxis("Horizontal");
-        rigidBody.AddForce(new Vector3(windDirection.x * windForce, windDirection.y * windForce, 0), ForceMode.Acceleration);
+        if (velocity > 1)
+            maxSpeed += 400 * Time.deltaTime / Mathf.Pow(maxSpeed, 1.1f);
+        else
+            maxSpeed = 10;
 
-        if (moveX != 0)
+
+        float piDeegre = transform.rotation.eulerAngles.y / 180 * Mathf.PI;
+        float wheelRotation = Input.GetAxis("Horizontal");
+        float anchorInput = Input.GetAxis("Anchor");
+
+        rotationForce = wheelRotation;
+
+        rigidBody.AddForce(new Vector3(windDirection.x * windForce, windDirection.y * windForce, 0), ForceMode.Acceleration);
+        velocity=rigidBody.velocity.magnitude;
+
+
+        rigidBody.AddForce(new Vector3(Mathf.Cos(piDeegre)*maxSpeed,0,-Mathf.Sin(piDeegre)*maxSpeed),ForceMode.Force);
+        if (wheelRotation != 0)// skret zalezny 
         {
-            rigidBody.AddForce(new Vector3(Mathf.Cos(piDeegre)*speed*moveX, 0, -Mathf.Sin(piDeegre) * speed * moveX), ForceMode.Acceleration);
+            maxSpeed = maxSpeed *(1-(Mathf.Abs(wheelRotation))/500);
+            transform.RotateAround(rotationPoint.transform.position, Vector3.up, roatationSpeed*Time.deltaTime*wheelRotation *velocity);
         }
-        if (moveY != 0) 
-        {
-            transform.RotateAround(rotationPoint.transform.position, Vector3.up * moveY, 10*Time.deltaTime);
-        }
+        
 
         }
 
