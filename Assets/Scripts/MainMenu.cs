5f2159f3e83022466c0c17e34f798fb8646e59e5
Tomasz Dąbrowski <51459629+DabToms@users.noreply.github.com>
2023-06-11 10:58:45 +0200

    Dodanie starego do main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,10 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class MAinMenu : MonoBehaviour
 {
-	// Start is called before the first frame update
-	public void StartGame()
-	{
-		StartCoroutine(ChangeSceneWithDelay());
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        StartCoroutine(ChangeSceneWithDelay());
 	}
 
 	// Update is called once per frame
@@ -28,8 +28,8 @@ public class MAinMenu : MonoBehaviour
 		//Print the time of when the function is first called.
 		Debug.Log("Started Coroutine at timestamp : " + Time.time);
 
-		//yield on a new YieldInstruction that waits for 5 seconds.
-		yield return new WaitForSeconds(4);
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(4);
 
 		// changed scene
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
