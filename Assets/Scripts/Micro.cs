b5df869fe72772a4ee5e033e4ef4899cdd77a38a
Tomasz Dąbrowski <51459629+DabToms@users.noreply.github.com>
2023-06-11 11:19:53 +0200

    Naprawa ClothSimulation w wuildzie i ui usunięcie prędkości z gry

--- Assets/Scripts/Micro.cs
+++ Assets/Scripts/Micro.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 [RequireComponent(typeof(AudioSource))]
 public class MicrophoneInput : MonoBehaviour
